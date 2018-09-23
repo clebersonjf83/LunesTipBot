10ce39eee77ae78042b93f42016019cb32295d92
clebersonjf83 <clebersonjf83@users.noreply.github.com>
2018-09-23 08:12:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ sudo pip3 install discord.py --upgrade
 
 sudo mkdir -p /opt/LunesTipBot
 
-sudo cp -prf ~/LunesTipBot/LunesDiscordTipBot /etc/init.d/LunesDiscordTipBot
-sudo cp -prf ~/LunesTipBot/LunesDiscordTipBot.py /opt/LunesTipBot/LunesDiscordTipBot.py
 sudo cp -prf ~/LunesTipBot/LunesTelegramTipBot /etc/init.d/LunesTelegramTipBot
 sudo cp -prf ~/LunesTipBot/LunesTelegramTipBot.py /opt/LunesTipBot/LunesTelegramTipBot.py
 sudo cp -prf ~/LunesTipBot/pickledb.py /opt/LunesTipBot/pickledb.py
